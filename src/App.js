73bf92c1454a16b09548bd6878a4c8549a3501fa
Aleksandr <blabin777@gmail.com>
2023-11-22 15:16:15 +0200

    22.11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
-import Video from "./components/Video"
+import Infoblock from "./components/Infoblock"
 function App() {
   return (
     <div className='App'>
       <Header />
       <Main />
+      <Infoblock />
     </div>
   )
 }
