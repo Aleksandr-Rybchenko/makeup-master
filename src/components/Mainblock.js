c32894b3b6aa576231c8b31dd2dc797c4531fd3c
Aleksandr <blabin777@gmail.com>
2023-12-08 19:16:50 +0200

    v2

--- src/components/Mainblock.js
+++ src/components/Mainblock.js
@@ -2,6 +2,12 @@ import styles from "./Mainblock.module.css"
 import firstImg from "./../img/lesson_one/1.png"
 import secondImg from "./../img/lesson_one/2.png"
 import threetImg from "./../img/lesson_one/3.png"
+import imgLessonTwo1 from "./../img/lesson_two/1.png"
+import imgLessonTwo2 from "./../img/lesson_two/2.png"
+import imgLessonTwo3 from "./../img/lesson_two/3.png"
+import imgLessonThree1 from "./../img/losseon_three/1.png"
+import imgLessonThree2 from "./../img/losseon_three/2.png"
+import imgLessonThree3 from "./../img/losseon_three/3.png"
 const Mainblock = () => {
   return (
     <div className={[styles.main_block]}>
@@ -52,6 +58,193 @@ const Mainblock = () => {
             <img src={secondImg} />
             <img src={threetImg} />
           </div>
+          <div className={[styles.lesson_slayder]}>{/* slayder */}</div>
+          {/* text */}
+          <p className={[styles.about_lesson_slayder]}>
+            На цьому уроці Ви навчитеся:
+          </p>
+          <div className={[styles.slayder_block_text]}>
+            <div>
+              <p className={[styles.lesson_text_slayder]}>
+                Створювати класичні голівудські форми
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Розбиратися в стайлінгових засобах і вибирати їх залежно від
+                типів волосся
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Схема та техніка завивки
+              </p>
+            </div>
+            <div>
+              <p className={[styles.lesson_text_slayder]}>
+                Оформлення волосся у лиця при наявності чубчика
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Секрет створення прикореневого об’єму на фен
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Правильно працювати плойкою з зажимом
+              </p>
+            </div>
+          </div>
+          <div className={[styles.strelka_parant]}>
+            <div className={styles.strelka}>
+              <a href='#'>Перейти до курсу</a>
+              <div className={[styles.strelka_svg]}>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='274'
+                  height='8'
+                  viewBox='0 0 274 8'
+                  fill='none'
+                >
+                  <path
+                    d='M274.355 4.35226C274.549 4.15629 274.548 3.83971 274.352 3.64516L271.159 0.474812C270.963 0.280264 270.646 0.281419 270.452 0.477393C270.257 0.673367 270.258 0.989947 270.454 1.1845L273.293 4.00258L270.475 6.84131C270.28 7.03729 270.281 7.35387 270.477 7.54841C270.673 7.74296 270.99 7.74181 271.184 7.54583L274.355 4.35226ZM0.00182481 5.5L274.002 4.5L273.998 3.5L-0.00182481 4.5L0.00182481 5.5Z'
+                    fill='#1B1818'
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          {/* 3 */}
+          <h3 className={[styles.lesson_number]}>Урок 3</h3>
+          <h3 className={[styles.lesson_title]}>Текстурний низький пучок</h3>
+          <p className={[styles.lesson_subtitle]}>
+            (На основі ломаної текстури)
+          </p>
+          <div className={[styles.lesson_video]}>
+            <iframe
+              width='1170'
+              height='650'
+              src='https://www.youtube.com/embed/_UQSoAdunIE?si=uK3bGUhcPWmE_H6_'
+              title='YouTube video player'
+              frameborder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className={[styles.lesson_img]}>
+            <img src={imgLessonTwo1} />
+            <img src={imgLessonTwo2} />
+            <img src={imgLessonTwo3} />
+          </div>
+          <div className={[styles.lesson_slayder]}>{/* slayder */}</div>
+          {/* text */}
+          <p className={[styles.about_lesson_slayder]}>
+            На цьому уроці Ви навчитеся:
+          </p>
+          <div className={[styles.slayder_block_text]}>
+            <div>
+              <p className={[styles.lesson_text_slayder_mod]}>
+                Унікальної техніки накрутки на утюжок
+              </p>
+            </div>
+            <div>
+              <p className={[styles.lesson_text_slayder_mod2]}>
+                Ломаної текстури яка створює прикореневий об’єм від кореня і вам
+                не доведеться працювати з прикореневою зоною окремо
+              </p>
+            </div>
+            <div>
+              <p className={[styles.lesson_text_slayder_mod]}>
+                Швидкої техніка збору зачіски
+              </p>
+            </div>
+          </div>
+          <div className={[styles.strelka_parant]}>
+            <div className={styles.strelka}>
+              <a href='#'>Перейти до курсу</a>
+              <div className={[styles.strelka_svg]}>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='274'
+                  height='8'
+                  viewBox='0 0 274 8'
+                  fill='none'
+                >
+                  <path
+                    d='M274.355 4.35226C274.549 4.15629 274.548 3.83971 274.352 3.64516L271.159 0.474812C270.963 0.280264 270.646 0.281419 270.452 0.477393C270.257 0.673367 270.258 0.989947 270.454 1.1845L273.293 4.00258L270.475 6.84131C270.28 7.03729 270.281 7.35387 270.477 7.54841C270.673 7.74296 270.99 7.74181 271.184 7.54583L274.355 4.35226ZM0.00182481 5.5L274.002 4.5L273.998 3.5L-0.00182481 4.5L0.00182481 5.5Z'
+                    fill='#1B1818'
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          {/* 4 */}
+          <h3 className={[styles.lesson_number]}>Урок 4</h3>
+          <h3 className={[styles.lesson_title]}>
+            Правила роботи з вирівнювачем
+          </h3>
+          <p className={[styles.lesson_subtitle]}>
+            (Об’ємні хвилі від обличчя)
+          </p>
+          <div className={[styles.lesson_video]}>
+            <iframe
+              width='1170'
+              height='650'
+              src='https://www.youtube.com/embed/ACUlQ6CdFpk?si=8-k1veltObVqcyRy'
+              title='YouTube video player'
+              frameborder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className={[styles.lesson_img]}>
+            <img src={imgLessonThree1} />
+            <img src={imgLessonThree2} />
+            <img src={imgLessonThree3} />
+          </div>
+          <div className={[styles.lesson_slayder]}>{/* slayder */}</div>
+          {/* text */}
+          <p className={[styles.about_lesson_slayder]}>
+            На цьому уроці Ви навчитеся:
+          </p>
+          <div className={[styles.slayder_block_text]}>
+            <div>
+              <p className={[styles.lesson_text_slayder]}>
+                Як робити швидкі укладання волосся за допомогою вирівнювача
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Особливостям роботи на важкому волоссі
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Навчитеся робити максимально комерційні укладання, які підійдуть
+                у будь-якій ситуації в салонах та на зйомках
+              </p>
+            </div>
+            <div>
+              <p className={[styles.lesson_text_slayder]}>
+                Об’єм він кореня до кінчиків
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Схема та техніка завивки, фінішна фіксація
+              </p>
+              <p className={[styles.lesson_text_slayder]}>
+                Трансформація оформлення у лиця
+              </p>
+            </div>
+          </div>
+          <div className={[styles.strelka_parant]}>
+            <div className={styles.strelka}>
+              <a href='#'>Перейти до курсу</a>
+              <div className={[styles.strelka_svg]}>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='274'
+                  height='8'
+                  viewBox='0 0 274 8'
+                  fill='none'
+                >
+                  <path
+                    d='M274.355 4.35226C274.549 4.15629 274.548 3.83971 274.352 3.64516L271.159 0.474812C270.963 0.280264 270.646 0.281419 270.452 0.477393C270.257 0.673367 270.258 0.989947 270.454 1.1845L273.293 4.00258L270.475 6.84131C270.28 7.03729 270.281 7.35387 270.477 7.54841C270.673 7.74296 270.99 7.74181 271.184 7.54583L274.355 4.35226ZM0.00182481 5.5L274.002 4.5L273.998 3.5L-0.00182481 4.5L0.00182481 5.5Z'
+                    fill='#1B1818'
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          {/* 4 */}
         </div>
       </div>
     </div>
